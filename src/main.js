b6f1d4a265c2bbea9c5318af98d621cf8a033dff
Nanka2402 <nastia.secr@gmail.com>
2024-01-25 23:53:56 +0200

    axios

--- src/main.js
+++ src/main.js
@@ -6,21 +6,26 @@ import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
 document.addEventListener('DOMContentLoaded', function () {
   const loaderContainer = document.getElementById('loader-container');
-  hideLoader(loaderContainer);
-
   const searchForm = document.getElementById('searchForm');
   const searchInput = document.getElementById('searchInput');
   const gallery = document.getElementById('gallery');
+  const loadMoreButton = document.getElementById('loadMoreButton');
 
   const apiKey = '41901564-aceebb7c9fdd08ac794ac72d8';
+  let currentPage = 1;
+  let totalHits = 0;
+  let currentSearchTerm = '';
 
-  searchForm.addEventListener('submit', function (e) {
+  hideLoader(loaderContainer);
+  loadMoreButton.style.display = 'none';
+
+  searchForm.addEventListener('submit', async function (e) {
     e.preventDefault();
-
-    // Відображення індикатора завантаження
-    showLoader(loaderContainer);
+    currentPage = 1;
+    loadMoreButton.style.display = 'none';
 
     const searchTerm = searchInput.value.trim();
     if (searchTerm === '') {
@@ -31,40 +36,70 @@ document.addEventListener('DOMContentLoaded', function () {
       hideLoader(loaderContainer);
       return;
     }
-
-    const apiUrl = 'https://pixabay.com/api/';
-    const requestData = {
-      key: apiKey,
-      q: searchTerm,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-      per_page: 9,
-    };
-
-    fetch(`${apiUrl}?${new URLSearchParams(requestData)}`)
-      .then(response => response.json())
-      .then(data => {
-        if (data.hits.length === 0) {
-          iziToast.error({
-            title: 'Error',
-            message: 'No images found for the provided search term',
-          });
-        } else {
-          displayImages(data.hits);
-        }
-      })
-      .catch(() => {
-        iziToast.error({
-          title: 'Error',
-          message: 'Failed to fetch images. Please try again later.',
-        });
-      })
-      .finally(() => {
-        hideLoader(loaderContainer);
-      });
+    currentSearchTerm = searchTerm;
+    showLoader(loaderContainer);
+    try {
+      await fetchData(currentSearchTerm);
+    } finally {
+      hideLoader(loaderContainer);
+    }
   });
 
+  async function fetchData(searchTerm) {
+    try {
+      const response = await axios.get('https://pixabay.com/api/', {
+        params: {
+          key: apiKey,
+          q: searchTerm,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
+          per_page: 40,
+          page: currentPage,
+        },
+      });
+
+      totalHits = response.data.totalHits; // Оновлюємо totalHits
+
+      if (response.data.hits.length === 0) {
+        iziToast.error({
+          title: 'Error',
+          message: 'No images found for the provided search term',
+        });
+      } else {
+        displayImages(response.data.hits);
+      }
+
+      if (currentPage * 40 >= totalHits) {
+        // Якщо кількість виведених зображень перевищує або рівна загальній кількості
+        loadMoreButton.style.display = 'none'; // Ховаємо кнопку
+        iziToast.info({
+          title: 'Info',
+          message: "We're sorry, but you've reached the end of search results.",
+        });
+      } else {
+        loadMoreButton.style.display = 'block'; // Показуємо кнопку
+      }
+    } catch (error) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Failed to fetch images. Please try again later.',
+      });
+    }
+  }
+
+  loadMoreButton.addEventListener('click', async function () {
+    currentPage++;
+    await fetchData(currentSearchTerm);
+    smoothScroll();
+  });
+  function smoothScroll() {
+    const cardHeight = document
+      .querySelector('.gallery-item')
+      .getBoundingClientRect().height;
+
+    window.scrollBy(0, cardHeight * 2);
+  }
   function showLoader(loaderContainer) {
     if (loaderContainer) {
       loaderContainer.style.display = 'block';
